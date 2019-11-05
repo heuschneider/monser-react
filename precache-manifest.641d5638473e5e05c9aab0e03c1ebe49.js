a73e61e92388959f6e90e2ddd757c25ce9f1546f
heuschneider <max.heuschneider@gmail.com>
2019-11-05 20:13:09 +0100

    Updates

---
+++ precache-manifest.641d5638473e5e05c9aab0e03c1ebe49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b8dc47e5ed6ee1d9083261e7adbd577",
+    "url": "/monser-react/index.html"
+  },
+  {
+    "revision": "58f7584188a14ade2cb6",
+    "url": "/monser-react/static/css/main.0fafd796.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monser-react/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "58f7584188a14ade2cb6",
+    "url": "/monser-react/static/js/main.d8480a68.chunk.js"
+  },
+  {
+    "revision": "7fba24f7444a5cb5ea64",
+    "url": "/monser-react/static/js/runtime-main.ac539ebc.js"
+  }
+]);
